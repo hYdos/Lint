1d3fee57676f593700f2e6c675f6fa62eb67f0d9
hYdos <haydenv06@gmail.com>
2020-04-02 07:18:50 +1000

    ty modmuss

--- src/main/java/me/hydos/lint/Lint.java
+++ src/main/java/me/hydos/lint/Lint.java
@@ -11,12 +11,14 @@ import me.hydos.techrebornApi.TechRebornApi;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.entity.EntityCategory;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import techreborn.client.GuiType;
@@ -24,7 +26,9 @@ import techreborn.client.GuiType;
 
 public class Lint implements ModInitializer {
 
-	public static final GuiType<AdventureTransformerBlockEntity> ADVENTURE_TRANSFORMER_GUI = GuiType.register(new Identifier("adventure_transformer"), () -> () -> (sync, player, be) -> new AdventureTransformerGui(sync, player, be));
+	public static final GuiType<AdventureTransformerBlockEntity> ADVENTURE_TRANSFORMER_GUI = GuiType.register(new Identifier("adventure_transformer"), () -> () -> AdventureTransformerGui::new);
+
+	public static final Identifier PLAY_DIMENSION_CHANGE_PACKET_ID = new Identifier("lint", "change_dimension");
 
 	public static final EntityType<LilTaterBattery> LIL_TATER =
 			Registry.register(
@@ -40,10 +44,25 @@ public class Lint implements ModInitializer {
 
 		TechRebornApi.registerBlock("lint", "adventure_transformer", AdventureTransformerRecipe.class, ItemGroup.TOOLS, AdventureTransformerBlockEntity::new, ADVENTURE_TRANSFORMER_GUI);
 
-		BlockRegister.registerBlocks(ItemGroup.BUILDING_BLOCKS, new Block(FabricBlockSettings.of(Material.EARTH).build()), "lively_grass");
-		BlockRegister.registerBlocks(ItemGroup.BUILDING_BLOCKS, new Block(FabricBlockSettings.of(Material.EARTH).build()), "rich_dirt");
+		BlockRegister.registerBlocks(ItemGroup.BUILDING_BLOCKS, new Block(FabricBlockSettings.of(Material.EARTH).hardness(0.5f).sounds(BlockSoundGroup.GRASS).build()), "lively_grass");
+		BlockRegister.registerBlocks(ItemGroup.BUILDING_BLOCKS, new Block(FabricBlockSettings.of(Material.EARTH).hardness(0.5f).sounds(BlockSoundGroup.GRASS).build()), "rich_dirt");
 		BiomeRegister.registerBiomes();
 		DimensionRegister.register();
+
+
+
+		ServerSidePacketRegistry.INSTANCE.register(PLAY_DIMENSION_CHANGE_PACKET_ID, (packetContext, packetByteBuf) ->{
+
+			String dimension = packetByteBuf.readString();
+			if(dimension.equalsIgnoreCase("HAYKAM")){
+				packetContext.getTaskQueue().execute(() -> {
+					packetContext.getPlayer().changeDimension(DimensionRegister.HAYKAM);
+					packetContext.getPlayer().openContainer(null);
+
+				});
+			}
+
+		});
 
 	}
 }
--- src/main/java/me/hydos/lint/blocks/AdventureTransformerGui.java
+++ src/main/java/me/hydos/lint/blocks/AdventureTransformerGui.java
@@ -1,16 +1,15 @@
 package me.hydos.lint.blocks;
 
+import io.netty.buffer.Unpooled;
+import me.hydos.lint.Lint;
 import me.hydos.lint.registers.DimensionRegister;
-import net.minecraft.block.BlockState;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.PacketByteBuf;
 import reborncore.client.containerBuilder.builder.BuiltContainer;
 import reborncore.client.gui.builder.GuiBase;
-import reborncore.client.gui.builder.widget.GuiButtonExtended;
-import reborncore.client.multiblock.Multiblock;
 
 public class AdventureTransformerGui extends GuiBase<BuiltContainer> {
     AdventureTransformerBlockEntity blockEntity;
@@ -19,12 +18,21 @@ public class AdventureTransformerGui extends GuiBase<BuiltContainer> {
     public AdventureTransformerGui(int syncID, PlayerEntity player, AdventureTransformerBlockEntity blockEntity) {
         super(player, blockEntity, blockEntity.createContainer(syncID, player));
         this.blockEntity = blockEntity;
+
+    }
+
+    @Override
+    public void init() {
+        super.init();
         dimensionButton = new ButtonWidget(0,0,100,10,"Enter tater land", (widget) ->{
             System.out.println("This was run");
-            MinecraftClient.getInstance().player.changeDimension(DimensionRegister.HAYKAM);
+
+            PacketByteBuf data = new PacketByteBuf(Unpooled.buffer());
+            data.writeString("HAYKAM");
+
+            ClientSidePacketRegistry.INSTANCE.sendToServer(Lint.PLAY_DIMENSION_CHANGE_PACKET_ID, data);
         });
         addButton(dimensionButton);
-
     }
 
     protected void drawBackground(float f, int mouseX, int mouseY) {
@@ -32,29 +40,11 @@ public class AdventureTransformerGui extends GuiBase<BuiltContainer> {
         Layer layer = Layer.BACKGROUND;
         this.drawSlot(8, 72, layer);
         dimensionButton.render(mouseX, mouseY, f);
-
-
     }
 
     protected void drawForeground(int mouseX, int mouseY) {
         super.drawForeground(mouseX, mouseY);
         Layer layer = Layer.FOREGROUND;
-        this.addHologramButton(6, 4, 212, layer).clickHandler(this::onClick);
-
         this.builder.drawMultiEnergyBar(this, 9, 19, (int) this.blockEntity.getEnergy(), (int) this.blockEntity.getMaxPower(), mouseX, mouseY, 0, layer);
-    }
-
-    public void onClick(GuiButtonExtended button, double mouseX, double mouseY) {
-        if (this.blockEntity.renderMultiblock == null) {
-            Multiblock multiblock = new Multiblock();
-            this.blockEntity.renderMultiblock = multiblock;
-        } else {
-            this.blockEntity.renderMultiblock = null;
-        }
-
-    }
-
-    public void addComponent(int x, int y, int z, BlockState blockState, Multiblock multiblock) {
-        multiblock.addComponent(new BlockPos(x - Direction.byId(this.blockEntity.getFacingInt()).getOffsetX() * 2, y, z - Direction.byId(this.blockEntity.getFacingInt()).getOffsetZ() * 2), blockState);
     }
 }
