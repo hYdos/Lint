d3a88b984cdf943c11c38f71900247fd6105dc67
hYdos <haydenv06@gmail.com>
2020-04-05 12:03:10 +1000

    Update TaterKingDungeonStructureStart.java

--- src/main/java/me/hydos/lint/taterkingdungeon/TaterKingDungeonStructureStart.java
+++ src/main/java/me/hydos/lint/taterkingdungeon/TaterKingDungeonStructureStart.java
@@ -2,12 +2,15 @@ package me.hydos.lint.taterkingdungeon;
 
 import net.minecraft.structure.StructureManager;
 import net.minecraft.structure.StructureStart;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.StructureFeature;
 
 public class TaterKingDungeonStructureStart extends StructureStart {
+
+    private static final Identifier BASE_POOL = new Identifier("lint:dungeon_pool");
 
     public TaterKingDungeonStructureStart(StructureFeature<?> feature, int chunkX, int chunkZ, BlockBox box, int references, long l) {
         super(feature, chunkX, chunkZ, box, references, l);
