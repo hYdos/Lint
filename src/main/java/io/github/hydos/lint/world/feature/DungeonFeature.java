348697927ef19aa5eebeff2fbc67b053a159220a
hydos <haydenv06@gmail.com>
2020-10-26 07:13:50 +1000

    alot more dungeon stuff

--- src/main/java/io/github/hydos/lint/world/feature/DungeonFeature.java
+++ src/main/java/io/github/hydos/lint/world/feature/DungeonFeature.java
@@ -1,6 +1,15 @@
 package io.github.hydos.lint.world.feature;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.StructureStart;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.gen.ChunkRandom;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.StructureConfig;
 import net.minecraft.world.gen.feature.JigsawFeature;
 import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
 
@@ -8,5 +17,21 @@ public class DungeonFeature extends JigsawFeature {
 
     public DungeonFeature(Codec<StructurePoolFeatureConfig> codec) {
         super(codec, 0, true, true);
+    }
+
+    @Override
+    public String getName() {
+        return "LintDungeon";
+    }
+
+    //TODO: make the entry the 1st object
+    @Override
+    public StructureStartFactory<StructurePoolFeatureConfig> getStructureStartFactory() {
+        return (feature, chunkX, chunkZ, boundingBox, references, seed) -> new Start(this, chunkX, chunkZ, boundingBox, references, seed);
+    }
+
+    @Override
+    public StructureStart<?> tryPlaceStart(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, BiomeSource biomeSource, StructureManager structureManager, long worldSeed, ChunkPos chunkPos, Biome biome, int referenceCount, ChunkRandom chunkRandom, StructureConfig structureConfig, StructurePoolFeatureConfig featureConfig) {
+        return super.tryPlaceStart(dynamicRegistryManager, chunkGenerator, biomeSource, structureManager, worldSeed, chunkPos, biome, referenceCount, chunkRandom, structureConfig, featureConfig);
     }
 }
--- src/main/java/io/github/hydos/lint/world/structure/DungeonGenerator.java
+++ src/main/java/io/github/hydos/lint/world/structure/DungeonGenerator.java
@@ -16,9 +16,18 @@ public class DungeonGenerator {
                 new StructurePool(Lint.id("dungeon"), new Identifier("empty"),
                         ImmutableList.of(
                                 Pair.of(Structures.createLegacySinglePoolElement("dungeon/dungeon_enterance", StructureProcessorLists.EMPTY), 50),
-                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/dungeon_prison_corner_connector", StructureProcessorLists.EMPTY), 50),
-                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/dungeon_staircase_split_connector", StructureProcessorLists.EMPTY), 50),
-                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/dungeon_staircase_split_connector_entrance", StructureProcessorLists.EMPTY), 50)),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/boss/king_tater", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/carpet_intersection", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/east_pathway", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/east_west_corner", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/hallway_north", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/intermediate_sized_carpet", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/intersection", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/large_carpet_stairs", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/prison_corner_connector", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/staircase_split_connector_enterance", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/stairs", StructureProcessorLists.EMPTY), 50),
+                                Pair.of(Structures.createLegacySinglePoolElement("dungeon/pathway/corner_north_east", StructureProcessorLists.EMPTY), 50)),
                         StructurePool.Projection.RIGID));
     }
 }
