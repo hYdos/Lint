d26290bb082084e59aedd59b82cbec4b6832ea7c
valoeghese <valoeghesemods@gmail.com>
2020-12-14 10:08:27 +1300

    Start basic floating island alg

--- src/main/java/io/github/hydos/lint/util/Voronoi.java
+++ src/main/java/io/github/hydos/lint/util/Voronoi.java
@@ -9,7 +9,7 @@ public final class Voronoi {
 		return new Vec2f(vx, vy);
 	}
 
-	public static Vec2f sample(float x, float y, int seed) {
+	public static Vec2f sampleBalanced(float x, float y, int seed) {
 		final int baseX = MathHelper.floor(x);
 		final int baseY = MathHelper.floor(y);
 		float rx = 0;
@@ -38,12 +38,39 @@ public final class Voronoi {
 		return new Vec2f(rx, ry);
 	}
 
+	public static double sampleFloating(float x, float y, int seed, OpenSimplexNoise noise) {
+		final int baseX = MathHelper.floor(x);
+		final int baseY = MathHelper.floor(y);
+		float rdist = 1000;
+
+		for (int xo = -1; xo <= 1; ++xo) {
+			int gridX = baseX + xo;
+
+			for (int yo = -1; yo <= 1; ++yo) {
+				int gridY = baseY + yo;
+
+				// if a floating island point
+				if (noise.sample(gridX * 0.2, gridY * 0.2) * 2 + random(gridX + 32, gridY, seed, 1) > 1.0) {
+					float vx = gridX + randomFloat(gridX, gridY, seed);
+					float vy = gridY + randomFloat(gridX, gridY, seed + 1);
+					float vdist = squaredDist(x, y, vx, vy);
+
+					if (vdist < rdist) {
+						rdist = vdist;
+					}
+				}
+			}
+		}
+
+		return Math.max(0.0, 1.0 - rdist);
+	}
+
 	private static int random(int x, int y, int seed, int mask) {
-		seed = 375462423 * seed + 672456235;
+		seed *= 375462423 * seed + 672456235;
 		seed += x;
-		seed = 375462423 * seed + 672456235;
+		seed *= 375462423 * seed + 672456235;
 		seed += y;
-		seed = 375462423 * seed + 672456235;
+		seed *= 375462423 * seed + 672456235;
 		return seed & mask;
 	}
 
