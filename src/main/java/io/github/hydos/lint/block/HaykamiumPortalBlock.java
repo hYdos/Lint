159903d57195b8a139b79b89d2d5cdafc41afd6f
hydos <haydenv06@gmail.com>
2020-12-23 17:51:20 +1000

    port 1.15 lint TeleportUtils.java

--- src/main/java/io/github/hydos/lint/block/HaykamiumPortalBlock.java
+++ src/main/java/io/github/hydos/lint/block/HaykamiumPortalBlock.java
@@ -1,11 +1,13 @@
 package io.github.hydos.lint.block;
 
+import io.github.hydos.lint.util.TeleportUtils;
 import io.github.hydos.lint.world.dimension.Dimensions;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.network.packet.s2c.play.StopSoundS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -21,14 +23,13 @@ public class HaykamiumPortalBlock extends Block {
 
 	@Override
 	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-		if(!world.isClient()){
-			ServerWorld haykamWorld = ((ServerWorld)world).getServer().getWorld(Dimensions.HAYKAM_WORLD);
+		if (!world.isClient()) {
+			ServerWorld haykamWorld = ((ServerWorld) world).getServer().getWorld(Dimensions.HAYKAM_WORLD);
 			if (haykamWorld == null) {
 				return;
 			}
-			FabricDimensions.teleport(entity, haykamWorld, new TeleportTarget(new Vec3d(0, 60, 0), new Vec3d(0,0,0), 0, 0));
-			entity.moveToWorld(haykamWorld);
-			if(entity instanceof ServerPlayerEntity){
+			TeleportUtils.teleport(((LivingEntity) entity), haykamWorld, new BlockPos(0, 80, 0));
+			if (entity instanceof ServerPlayerEntity) {
 				ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) entity;
 				serverPlayerEntity.networkHandler.sendPacket(new StopSoundS2CPacket());
 			}
