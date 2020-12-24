ccb591898124616af32cd14270ccf46ceeeddc2e
hydos <haydenv06@gmail.com>
2020-12-24 20:03:42 +1000

    fix valos mistakes & some json

--- src/main/java/me/hydos/lint/mixin/EntityMixin.java
+++ src/main/java/me/hydos/lint/mixin/EntityMixin.java
@@ -7,17 +7,31 @@ import net.minecraft.tag.FluidTags;
 import net.minecraft.tag.Tag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public class EntityMixin {
 
+	private FluidState currentFluid;
+
 	@Redirect(method = "updateMovementInFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/fluid/FluidState;isIn(Lnet/minecraft/tag/Tag;)Z"))
 	private boolean isIn(FluidState state, Tag<Fluid> tag) {
+		this.currentFluid = state;
 		if (!state.getFluid().isIn(tag) && !tag.equals(FluidTags.LAVA)) {
 			return !state.isEmpty();
 		} else {
 			return state.isIn(tag);
+		}
+	}
+
+	@Inject(method = "onSwimmingStart", at = @At("HEAD"), cancellable = true)
+	private void checkIfIsWater(CallbackInfo ci){
+		if(currentFluid != null){
+			if(!currentFluid.isIn(FluidTags.WATER)){
+				ci.cancel();
+			}
 		}
 	}
 }
