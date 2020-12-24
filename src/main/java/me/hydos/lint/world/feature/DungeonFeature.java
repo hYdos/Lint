2a9c826a093f63c677feca015a8fcb1ffad2aec6
valoeghese <valoeghesemods@gmail.com>
2020-12-24 22:22:31 +1300

    dungeons below ground

--- src/main/java/me/hydos/lint/world/feature/DungeonFeature.java
+++ src/main/java/me/hydos/lint/world/feature/DungeonFeature.java
@@ -1,8 +1,11 @@
 package me.hydos.lint.world.feature;
 
 import com.mojang.serialization.Codec;
+
 import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.StructurePiece;
 import net.minecraft.structure.StructureStart;
+import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.world.biome.Biome;
@@ -14,19 +17,33 @@ import net.minecraft.world.gen.feature.JigsawFeature;
 import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
 
 public class DungeonFeature extends JigsawFeature {
+	public DungeonFeature(Codec<StructurePoolFeatureConfig> codec) {
+		super(codec, 0, true, true);
+	}
 
-    public DungeonFeature(Codec<StructurePoolFeatureConfig> codec) {
-        super(codec, 0, true, true);
-    }
+	//TODO: make the entry the 1st object
+	@Override
+	public StructureStartFactory<StructurePoolFeatureConfig> getStructureStartFactory() {
+		return (feature, chunkX, chunkZ, boundingBox, references, seed) -> new Start(this, chunkX, chunkZ, boundingBox, references, seed);
+	}
 
-    //TODO: make the entry the 1st object
-    @Override
-    public StructureStartFactory<StructurePoolFeatureConfig> getStructureStartFactory() {
-        return (feature, chunkX, chunkZ, boundingBox, references, seed) -> new Start(this, chunkX, chunkZ, boundingBox, references, seed);
-    }
+	@Override
+	public StructureStart<?> tryPlaceStart(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, BiomeSource biomeSource, StructureManager structureManager, long worldSeed, ChunkPos chunkPos, Biome biome, int referenceCount, ChunkRandom chunkRandom, StructureConfig structureConfig, StructurePoolFeatureConfig featureConfig) {
+		return super.tryPlaceStart(dynamicRegistryManager, chunkGenerator, biomeSource, structureManager, worldSeed, chunkPos, biome, referenceCount, chunkRandom, structureConfig, featureConfig);
+	}
 
-    @Override
-    public StructureStart<?> tryPlaceStart(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, BiomeSource biomeSource, StructureManager structureManager, long worldSeed, ChunkPos chunkPos, Biome biome, int referenceCount, ChunkRandom chunkRandom, StructureConfig structureConfig, StructurePoolFeatureConfig featureConfig) {
-        return super.tryPlaceStart(dynamicRegistryManager, chunkGenerator, biomeSource, structureManager, worldSeed, chunkPos, biome, referenceCount, chunkRandom, structureConfig, featureConfig);
-    }
+	public static class Start extends JigsawFeature.Start {
+		public Start(DungeonFeature dungeonFeature, int chunkX, int chunkZ, BlockBox boundingBox, int references, long seed) {
+			super(dungeonFeature, chunkX, chunkZ, boundingBox, references, seed);
+		}
+
+		@Override
+		public void init(DynamicRegistryManager dynamicRegistryManager, ChunkGenerator chunkGenerator, StructureManager structureManager, int i, int j, Biome biome, StructurePoolFeatureConfig structurePoolFeatureConfig) {
+			super.init(dynamicRegistryManager, chunkGenerator, structureManager, i, j, biome, structurePoolFeatureConfig);
+
+			for (StructurePiece piece : this.children) {
+				piece.translate(0, -16, 0);
+			}
+		}
+	}
 }
--- src/main/java/me/hydos/lint/world/structure/ConfiguredStructureFeatures.java
+++ src/main/java/me/hydos/lint/world/structure/ConfiguredStructureFeatures.java
@@ -7,7 +7,6 @@ import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraft.world.gen.feature.StructurePoolFeatureConfig;
 
 public interface ConfiguredStructureFeatures {
-
     ConfiguredStructureFeature<StructurePoolFeatureConfig, ? extends StructureFeature<StructurePoolFeatureConfig>> DUNGEON = register("dungeon", Structures.DUNGEON.configure(new StructurePoolFeatureConfig(() -> DungeonGenerator.structures, 7)));
 
     static <FC extends FeatureConfig, F extends StructureFeature<FC>> ConfiguredStructureFeature register(String id, ConfiguredStructureFeature<FC, F> configuredStructureFeature) {
