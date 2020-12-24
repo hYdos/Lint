e6edfc754aa4616e869194e9fc42203f9b3a398b
hydos <haydenv06@gmail.com>
2020-12-24 15:27:16 +1000

    add sicieron armor

--- src/main/java/me/hydos/lint/item/Items.java
+++ src/main/java/me/hydos/lint/item/Items.java
@@ -1,7 +1,10 @@
 package me.hydos.lint.item;
 
 import me.hydos.lint.Lint;
+import me.hydos.lint.item.armor.ArmorMaterials;
 import me.hydos.lint.item.group.ItemGroups;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Rarity;
@@ -15,19 +18,34 @@ public class Items {
 	public static final Item TATER_ESSENCE = new TaterEssenceItem(new Item.Settings().group(ItemGroup.MATERIALS).rarity(Rarity.EPIC).maxCount(1));
 
 	/**
-	 * Armor
+	 * Armor Sets
 	 */
-
+	public static final Item SICIERON_HELMET = new ArmorItem(ArmorMaterials.SICIERON, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroups.TOOLS));
+	public static final Item SICIERON_CHESTPLATE = new ArmorItem(ArmorMaterials.SICIERON, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroups.TOOLS));
+	public static final Item SICIERON_LEGGINGS = new ArmorItem(ArmorMaterials.SICIERON, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroups.TOOLS));
+	public static final Item SICIERON_BOOTS = new ArmorItem(ArmorMaterials.SICIERON, EquipmentSlot.FEET, new Item.Settings().group(ItemGroups.TOOLS));
 
 	/**
 	 * Ore Materials
 	 */
-	public static final Item SICIERON_INGOT = new Item(new Item.Settings().group(ItemGroup.MATERIALS).maxCount(64));
-	public static final Item TARSCAN_SHARD = new Item(new Item.Settings().group(ItemGroup.MATERIALS).maxCount(64));
-	public static final Item JUREL_POWDER = new Item(new Item.Settings().group(ItemGroup.MATERIALS).rarity(Rarity.RARE).maxCount(32));
+	public static final Item SICIERON_INGOT = new Item(new Item.Settings().group(ItemGroups.ITEMS).maxCount(64));
+	public static final Item TARSCAN_SHARD = new Item(new Item.Settings().group(ItemGroups.ITEMS).maxCount(64));
+	public static final Item JUREL_POWDER = new Item(new Item.Settings().group(ItemGroups.ITEMS).rarity(Rarity.RARE).maxCount(64));
 
 	public static void register() {
 		ItemGroups.register();
+		registerOreMaterials();
+		registerArmorSets();
+	}
+
+	private static void registerArmorSets() {
+		Registry.register(Registry.ITEM, Lint.id("sicieron_helmet"), SICIERON_HELMET);
+		Registry.register(Registry.ITEM, Lint.id("sicieron_chestplate"), SICIERON_CHESTPLATE);
+		Registry.register(Registry.ITEM, Lint.id("sicieron_leggings"), SICIERON_LEGGINGS);
+		Registry.register(Registry.ITEM, Lint.id("sicieron_boots"), SICIERON_BOOTS);
+	}
+
+	private static void registerOreMaterials() {
 		Registry.register(Registry.ITEM, Lint.id("tater_essence"), TATER_ESSENCE);
 		Registry.register(Registry.ITEM, Lint.id("sicieron_ingot"), SICIERON_INGOT);
 		Registry.register(Registry.ITEM, Lint.id("jurel_powder"), JUREL_POWDER);
--- src/main/java/me/hydos/lint/item/group/ItemGroups.java
+++ src/main/java/me/hydos/lint/item/group/ItemGroups.java
@@ -1,6 +1,7 @@
 package me.hydos.lint.item.group;
 
 import me.hydos.lint.Lint;import me.hydos.lint.block.Blocks;
+import me.hydos.lint.item.Items;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
@@ -8,14 +9,24 @@ import net.minecraft.item.ItemStack;
 
 public class ItemGroups {
 
-	public static final ItemGroup LINT_BLOCKS = FabricItemGroupBuilder.create(
+	public static final ItemGroup BLOCKS = FabricItemGroupBuilder.create(
 			Lint.id("lint_blocks"))
 			.icon(() -> new ItemStack(Blocks.LIVELY_GRASS))
 			.build();
 
-	public static final ItemGroup LINT_DECORATIONS = FabricItemGroupBuilder.create(
+	public static final ItemGroup DECORATIONS = FabricItemGroupBuilder.create(
 			Lint.id("lint_decorations"))
 			.icon(() -> new ItemStack(Blocks.MYSTICAL_DAISY))
+			.build();
+
+	public static final ItemGroup TOOLS = FabricItemGroupBuilder.create(
+			Lint.id("lint_tools"))
+			.icon(() -> new ItemStack(Blocks.MYSTICAL_DAISY))
+			.build();
+
+	public static final ItemGroup ITEMS = FabricItemGroupBuilder.create(
+			Lint.id("lint_items"))
+			.icon(() -> new ItemStack(Items.TATER_ESSENCE))
 			.build();
 
 	public static void register() {
