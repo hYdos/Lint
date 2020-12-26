206136ad75daa207e026c3a70caf0276a8ad3723
Ramid Khan <ramidzkh@gmail.com>
2020-12-26 21:56:00 +1100

    Hydos can't code

--- src/main/java/me/hydos/lint/mixin/client/ModelLoaderMixin.java
+++ src/main/java/me/hydos/lint/mixin/client/ModelLoaderMixin.java
@@ -10,13 +10,22 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ModelLoader.class)
 public class ModelLoaderMixin {
 
-	@Redirect(method = "loadModel", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
-	private void silentFluidBlockstateErrors(Logger logger, String message, Object p0, Object p1){
-		if(p1 instanceof ModelIdentifier){
+	@Redirect(
+			method = "method_21604", // Lambda method
+			at = @At(
+					value = "INVOKE",
+					target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"),
+			expect = 0 // This mixin is optional
+	)
+	private void silenceMissingFluidBlockstates(Logger logger, String message, Object p0, Object p1) {
+		if (p1 instanceof ModelIdentifier) {
 			ModelIdentifier location = (ModelIdentifier) p1;
-			if(location.getNamespace().equals("lint") && location.getVariant().startsWith("level=")) { // Must not be a fluid. warn about it
-				logger.error(message, p0, p1);
+
+			if (location.getNamespace().equals("lint") && location.getVariant().startsWith("level=")) { // Must not be a fluid. warn about it
+				return;
 			}
 		}
+
+		logger.error(message, p0, p1);
 	}
 }
