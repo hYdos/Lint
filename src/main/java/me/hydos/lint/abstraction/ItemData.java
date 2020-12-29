0d7c97aedc3c3a0cfa8b2dc3f1f3696171f848c4
valoeghese <valoeghesemods@gmail.com>
2020-12-29 23:24:36 +1300

    this build will fail but it at least doesn't have half written code

---
+++ src/main/java/me/hydos/lint/abstraction/ItemData.java
@@ -0,0 +1,17 @@
+package me.hydos.lint.abstraction;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class ItemData {
+	public Key register(Identifier id) {
+		
+	}
+	
+	public static class Key {
+		public ItemData get(ItemStack stack) {
+			
+		}
+	}
+}
+
