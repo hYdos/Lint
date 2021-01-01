21a6dec63f3614c9db975cc890920c0ad27410e8
valoeghese <valoeghesemods@gmail.com>
2021-01-01 22:23:26 +1300

    start structure code

---
+++ src/main/java/me/hydos/lint/world/structure2/Room.java
@@ -0,0 +1,32 @@
+/*
+ * Lint
+ * Copyright (C) 2020 hYdos, Valoeghese, ramidzkh
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package me.hydos.lint.world.structure2;
+
+import java.util.List;
+import java.util.Random;
+
+import me.hydos.lint.world.PseudoWorld;
+import net.minecraft.util.math.Box;
+
+public abstract class Room {
+	protected abstract void generate(PseudoWorld world, Random rand, final Box box);
+	protected abstract Box getBounds(Random rand);
+	protected abstract List<Room> computeNodes(Box box, Random rand);
+}
