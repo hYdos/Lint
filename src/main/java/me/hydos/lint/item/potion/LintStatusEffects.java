cbd450ce41739afd30ddb8047494eff71758e35f
Ramid Khan <ramidzkh@gmail.com>
2021-03-14 22:06:42 +1100

    Transmutation status effect and potions

---
+++ src/main/java/me/hydos/lint/item/potion/LintStatusEffects.java
@@ -0,0 +1,34 @@
+/*
+ * Lint
+ * Copyright (C) 2020 hYdos, Valoeghese, ramidzkh
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package me.hydos.lint.item.potion;
+
+import me.hydos.lint.Lint;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.util.registry.Registry;
+
+public class LintStatusEffects {
+
+	public static final StatusEffect TRANSMUTATION = register("transmutation", new TransmutationStatusEffect(StatusEffectType.NEUTRAL, 0x50C040));
+
+	private static StatusEffect register(String name, StatusEffect effect) {
+		return Registry.register(Registry.STATUS_EFFECT, Lint.id(name), effect);
+	}
+}
