361e8514bbd053d1c31cf20e12b36539b1d8dc48
i509VCB <git@i509.me>
2021-04-09 14:30:22 -0500

    Kinda of implement i5 levitation

--- src/main/java/me/hydos/lint/entity/aggressive/I509VCBEntity.java
+++ src/main/java/me/hydos/lint/entity/aggressive/I509VCBEntity.java
@@ -22,8 +22,10 @@ package me.hydos.lint.entity.aggressive;
 import me.hydos.lint.bossbar.ModernBossBar;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.RangedAttackMob;
+import net.minecraft.entity.ai.control.FlightMoveControl;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.ai.pathing.BirdNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -35,28 +37,40 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.TextColor;
 import net.minecraft.world.World;
 
-public class I509VCBEntity extends HostileEntity implements RangedAttackMob {
+// TODO: Why does the boss stop levitating?
+public class I509VCBEntity extends HostileEntity {
 	public I509VCBEntity(EntityType<? extends HostileEntity> entityType, World world) {
 		super(entityType, world);
 		ModernBossBar.getInstance().setTitle(getDisplayName().shallowCopy().styled(style -> style.withBold(true).withColor(TextColor.fromRgb(0xB41917))));
+		this.moveControl = new FlightMoveControl(this, 2, false);
+		this.setNoGravity(true);
 	}
 
 	public static DefaultAttributeContainer.Builder createAttributes() {
 		return LivingEntity.createLivingAttributes()
-				.add(EntityAttributes.GENERIC_MAX_HEALTH, 125)
-				.add(EntityAttributes.GENERIC_FOLLOW_RANGE, 12)
-				.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 20);
+				.add(EntityAttributes.GENERIC_MAX_HEALTH, 125.0D)
+				.add(EntityAttributes.GENERIC_FOLLOW_RANGE, 12.0D)
+				.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0D) // Yes this is temporary
+				.add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.0D)
+				.add(EntityAttributes.GENERIC_FLYING_SPEED, 2.0D);
 	}
 
 	@Override
 	protected void initGoals() {
 		this.goalSelector.add(1, new SwimGoal(this));
+		this.goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 28.0F));
+		this.goalSelector.add(2, new MeleeAttackGoal(this, 1D, false));
+
 		this.targetSelector.add(1, new RevengeGoal(this));
-		this.goalSelector.add(2, new ProjectileAttackGoal(this, 1.0D, 40, 40.0F));
 		this.targetSelector.add(2, new FollowTargetGoal<>(this, PlayerEntity.class, false));
-		this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
-		this.goalSelector.add(3, new MeleeAttackGoal(this, 1D, false));
-		this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
+	}
+
+	@Override
+	public EntityNavigation createNavigation(World world) {
+		BirdNavigation flyingNav = new BirdNavigation(this, world);
+		flyingNav.setCanSwim(true);
+		flyingNav.setSpeed(100);
+		return flyingNav;
 	}
 
 	@Override
@@ -75,18 +89,31 @@ public class I509VCBEntity extends HostileEntity implements RangedAttackMob {
 	}
 
 	@Override
-	public void attack(LivingEntity target, float pullProgress) {
-//		target.kill(); //the power of a mod rationer
+	public void tick() {
+		super.tick();
 	}
 
 	@Override
 	protected void mobTick() {
+		super.mobTick();
+
 		ModernBossBar.getInstance().setEndX(ModernBossBar.calculateEndX(KingTaterEntity.getScaledHealth(getHealth(), getMaxHealth())));
+
 		if (hasStatusEffect(StatusEffects.JUMP_BOOST)) {
 			addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20, 4, true, true, true, null));
 		}
+
 		if (hasStatusEffect(StatusEffects.REGENERATION)) {
 			addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20, 0, true, true, true, null));
 		}
+	}
+
+	@Override
+	public boolean damage(DamageSource source, float amount) {
+		if (source == DamageSource.FALL) {
+			return false;
+		}
+
+		return super.damage(source, amount);
 	}
 }
