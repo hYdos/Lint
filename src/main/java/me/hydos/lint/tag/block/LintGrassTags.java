98894ed7ba2488663cabcb66ea13148679b772bc
Tehc <18603248+TehcJS@users.noreply.github.com>
2021-10-17 02:50:08 -0400

    Revert "Revert "fix worldgenjson't caves ""

--- src/main/java/me/hydos/lint/tag/block/LintGrassTags.java
+++ src/main/java/me/hydos/lint/tag/block/LintGrassTags.java
@@ -17,17 +17,18 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package me.hydos.lint.tag;
+package me.hydos.lint.tag.block;
 
 import me.hydos.lint.Lint;
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.tag.Tag;
 
-public class LintBlockTags {
+public final class LintGrassTags {
+	public static final Tag<Block> UNTAINTED = TagFactory.BLOCK.create(Lint.id("untainted/grass"));
 
-	public static final Tag<Block> BASIC_CASING = TagRegistry.block(Lint.id("basic_smeltery_casing"));
+	private LintGrassTags() {}
 
-	public static void initialize() {
-	}
+	/* no-op */
+	static void initialize() {}
 }
